c2c290e99ef9bfc70cd321c5bbc1a1a3b616c424
pikai <51418996@qq.com>
2017-06-01 16:37:33 +0800

    添加说明文件how-to-use

--- mybatis-utils-mgb/src/main/java/mgb/utils/builder/XmlConfigBuilder.java
+++ mybatis-utils-mgb/src/main/java/mgb/utils/builder/XmlConfigBuilder.java
@@ -19,18 +19,18 @@ import java.util.List;
 /**
  * MGB配置文件解析器
  */
-        public class XmlConfigBuilder {
+public class XmlConfigBuilder {
 
-            private final static Logger LOGGER = LoggerFactory.getLogger(XmlConfigBuilder.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(XmlConfigBuilder.class);
 
-            private Configuration config;
+    private Configuration config;
 
-            private DbAccess dbAccess;
+    private DbAccess dbAccess;
 
-            private DataSource dataSource;
+    private DataSource dataSource;
 
-            public Configuration getConfig() {
-                return config;
+    public Configuration getConfig() {
+        return config;
     }
 
     public void setConfig(Configuration config) {
@@ -38,14 +38,14 @@ import java.util.List;
     }
 
     //构造自定义的DbAccess组件
-    public XmlConfigBuilder(Configuration config,DbAccess dbAccess,DataSource dataSource) {
+    public XmlConfigBuilder(Configuration config, DbAccess dbAccess, DataSource dataSource) {
         this.config = config;
         this.dbAccess = dbAccess;
         this.dataSource = dataSource;
     }
 
     //默认构造DefaultDbAccess实例
-    public XmlConfigBuilder(Configuration config,DataSource dataSource) {
+    public XmlConfigBuilder(Configuration config, DataSource dataSource) {
         this.config = config;
         this.dataSource = dataSource;
         this.dbAccess = new DefaultDbAccess(dataSource);
@@ -59,7 +59,7 @@ import java.util.List;
         File file = FileUtils.getFile(config.getJarPath());
         String errorMsg = "找不到指定的JDBC驱动包文件";
 
-        if (file == null) {
+        if (file.exists()) {
             if (LOGGER.isDebugEnabled()) {
                 LOGGER.debug(errorMsg);
             }
@@ -69,7 +69,7 @@ import java.util.List;
         //验证指定的projectPath是否存在
         errorMsg = "找不到指定的项目路径";
         file = FileUtils.getFile(config.getProjectPath());
-        if (file == null) {
+        if (file.exists()) {
             if (LOGGER.isDebugEnabled()) {
                 LOGGER.debug(errorMsg);
             }
@@ -77,66 +77,66 @@ import java.util.List;
         }
 
 
-            List<String> tables = this.dbAccess.getTables();
-            for (String name : tables) {
-                if (LOGGER.isDebugEnabled()){
-                    LOGGER.debug(name);
-                }
-            String className = name.substring(0, 1).toUpperCase()+name.substring(1);
+        List<String> tables = this.dbAccess.getTables();
+        for (String name : tables) {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(name);
+            }
+            String className = name.substring(0, 1).toUpperCase() + name.substring(1);
 
             Element root = DocumentHelper.createElement("generatorConfiguration");
             Document document = DocumentHelper.createDocument(root);
             //添加<!DOCTYPE generatorConfiguration PUBLIC "-//mybatis.org//DTD MyBatis Generator Configuration 1.0//EN" "http://mybatis.org/dtd/mybatis-generator-config_1_0.dtd" >
-            document.addDocType("generatorConfiguration","-//mybatis.org//DTD MyBatis Generator Configuration 1.0//EN","http://mybatis.org/dtd/mybatis-generator-config_1_0.dtd");
+            document.addDocType("generatorConfiguration", "-//mybatis.org//DTD MyBatis Generator Configuration 1.0//EN", "http://mybatis.org/dtd/mybatis-generator-config_1_0.dtd");
 
             //构建classPathEntry节点
             Element classPathEntryNode = root.addElement("classPathEntry");
-            classPathEntryNode.addAttribute("location",config.getJarPath());
+            classPathEntryNode.addAttribute("location", config.getJarPath());
 
             //构建context节点
             Element contextNode = root.addElement("context");
-            contextNode.addAttribute("id","context1");
-            contextNode.addAttribute("targetRuntime","MyBatis3");
+            contextNode.addAttribute("id", "context1");
+            contextNode.addAttribute("targetRuntime", "MyBatis3");
 
             Element commentGeneratorNode = contextNode.addElement("commentGenerator");
-            commentGeneratorNode.addElement("property").addAttribute("name","suppressAllComments")
-                    .addAttribute("value","true");
+            commentGeneratorNode.addElement("property").addAttribute("name", "suppressAllComments")
+                    .addAttribute("value", "true");
 
             //构建jdbcConnection节点
             Element jdbcConnectionNode = contextNode.addElement("jdbcConnection");
-            jdbcConnectionNode.addAttribute("driverClass",config.getDriverClass())
-                    .addAttribute("connectionURL",config.getConnectionURL())
-                    .addAttribute("userId",config.getUserId())
-                    .addAttribute("password",config.getPassword());
+            jdbcConnectionNode.addAttribute("driverClass", config.getDriverClass())
+                    .addAttribute("connectionURL", config.getConnectionURL())
+                    .addAttribute("userId", config.getUserId())
+                    .addAttribute("password", config.getPassword());
 
             //构建javaModelGenerator节点
             Element javaModelGeneratorNode = contextNode.addElement("javaModelGenerator");
-            javaModelGeneratorNode.addAttribute("targetProject",config.getProjectPath())
-                    .addAttribute("targetPackage",config.getModelPackage());
+            javaModelGeneratorNode.addAttribute("targetProject", config.getProjectPath())
+                    .addAttribute("targetPackage", config.getModelPackage());
 
             //构建javaModelGenerator节点
             Element sqlMapGeneratorNode = contextNode.addElement("sqlMapGenerator");
-            sqlMapGeneratorNode.addAttribute("targetProject",config.getProjectPath())
-                    .addAttribute("targetPackage",config.getMapperPackage());
+            sqlMapGeneratorNode.addAttribute("targetProject", config.getProjectPath())
+                    .addAttribute("targetPackage", config.getMapperPackage());
 
             //构建javaClientGenerator节点
             Element javaClientGeneratorNode = contextNode.addElement("javaClientGenerator");
-            javaClientGeneratorNode.addAttribute("targetProject",config.getProjectPath())
-                    .addAttribute("targetPackage",config.getDaoPackage())
-                    .addAttribute("type","XMLMAPPER");
+            javaClientGeneratorNode.addAttribute("targetProject", config.getProjectPath())
+                    .addAttribute("targetPackage", config.getDaoPackage())
+                    .addAttribute("type", "XMLMAPPER");
 
             //构建table节点
             Element tableNode = contextNode.addElement("table");
             //动态赋值表名和类名
-            tableNode.addAttribute("tableName",name)
-                    .addAttribute("domainObjectName",className)
-                    .addAttribute("enableCountByExample","false")
-                    .addAttribute("enableUpdateByExample","false")
-                    .addAttribute("enableDeleteByExample","false")
-                    .addAttribute("enableSelectByExample","false")
-                    .addAttribute("selectByExampleQueryId","false");
-            tableNode.addElement("property").addAttribute("name","useActualColumnNames")
-                    .addAttribute("value","true");
+            tableNode.addAttribute("tableName", name)
+                    .addAttribute("domainObjectName", className)
+                    .addAttribute("enableCountByExample", "false")
+                    .addAttribute("enableUpdateByExample", "false")
+                    .addAttribute("enableDeleteByExample", "false")
+                    .addAttribute("enableSelectByExample", "false")
+                    .addAttribute("selectByExampleQueryId", "false");
+            tableNode.addElement("property").addAttribute("name", "useActualColumnNames")
+                    .addAttribute("value", "true");
 
             //MGB代码生成
             List<String> warnings = new ArrayList<String>();
@@ -148,7 +148,7 @@ import java.util.List;
             MyBatisGenerator myBatisGenerator = new MyBatisGenerator(configuration, callback, warnings);
             myBatisGenerator.generate(null);
 
-            if (LOGGER.isDebugEnabled()){
+            if (LOGGER.isDebugEnabled()) {
                 LOGGER.debug("db_table:" + name + "------java_bean:" + className);
             }
 
@@ -159,7 +159,7 @@ import java.util.List;
 
         long endTime = System.currentTimeMillis();
 
-        if (LOGGER.isDebugEnabled()){
+        if (LOGGER.isDebugEnabled()) {
             LOGGER.debug(endTime - startTime + "ms used!");
         }
     }
